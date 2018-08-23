66cedcf055718ddfbe1902d4b3cb06fbd6067217
Cassiano Raupp <cassiano.raupp@zeeng.com.br>
2018-08-23 13:12:44 -0300

    Ajustes.

--- src/Curriculo.js
+++ src/Curriculo.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Content from './Content';
+import Footer from './Footer';
 
 class Curriculo extends Component {
   render() {
     return (
-      <Content></Content>
+      <div>
+        <Content></Content>
+        <Footer></Footer>
+      </div>
     )
   }
 }
