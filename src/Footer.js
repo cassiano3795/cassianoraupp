5bcb3093ea1b8cac4650a384c5fb00ce96c18c64
Cassiano Raupp <cassiano.raupp@zeeng.com.br>
2018-08-23 13:15:43 -0300

    Ajustes

--- src/Footer.js
+++ src/Footer.js
@@ -5,9 +5,9 @@ class Footer extends Component {
         return (
             <footer className="w3-container w3-teal w3-center w3-margin-top">
                 <p>Encontre-me nas Rede Sociais.</p>
-                <a href="https://www.facebook.com/csraupp" target="_blank"><i className="fa fa-facebook-official w3-hover-opacity" /></a>
-                <a href="https://www.instagram.com/rauppcs/?hl=pt-br" target="_blank"><i className="fa fa-instagram w3-hover-opacity" /></a>
-                <a href="https://www.linkedin.com/in/cassiano-raupp-50a6a9133/" target="_blank"><i className="fa fa-linkedin w3-hover-opacity" /></a>
+                <a href="https://www.facebook.com/csraupp" target="_blank" ><i className="fa fa-facebook-official w3-hover-opacity" /></a>
+                <a href="https://www.instagram.com/rauppcs/?hl=pt-br" style={{marginLeft: 5}} target="_blank"><i className="fa fa-instagram w3-hover-opacity" /></a>
+                <a href="https://www.linkedin.com/in/cassiano-raupp-50a6a9133/" style={{marginLeft: 5}} target="_blank"><i className="fa fa-linkedin w3-hover-opacity" /></a>
             </footer>
         )
     }
