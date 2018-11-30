df83f5fe10532904eca7cd4c8c8dcfc6ff9dc366
Cassiano Raupp <cassiano.raupp@outlook.com>
2018-11-30 09:36:16 -0200

    Alteração da imagem.

--- src/Imagem.js
+++ src/Imagem.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+const infos = require('./dados.json')
 
 class Imagem extends Component {
     render() {
         return (
             <div className="w3-display-container">
-                <img src="https://success.salesforce.com/resource/1498435200000/sharedlayout/img/new-user-image-default.png" style={{ width: '100%' }} alt="Avatar" />
-                <div className="w3-display-bottomleft w3-container w3-text-black">
+                <img src={infos.imagem} style={{ width: '100%' }} alt="Avatar" />
+                <div className="w3-display-bottomleft w3-container w3-text-white">
                     <h2>Cassiano Raupp</h2>
                 </div>
             </div>
