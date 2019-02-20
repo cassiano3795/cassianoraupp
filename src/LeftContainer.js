fb398f493c66dd19d817209e1e3b8f95cd3d608e
Cassiano Raupp <cassiano.raupp@outlook.com>
2019-02-19 22:37:24 -0300

    Atualizações.

--- src/LeftContainer.js
+++ src/LeftContainer.js
@@ -23,14 +23,14 @@ class LeftContainer extends Component {
                 })}
 
                 <br />
-                <p className="w3-large w3-text-theme"><b><i className="fa fa-globe fa-fw w3-margin-right w3-text-teal" />Idiomas</b></p>
+                {/* <p className="w3-large w3-text-theme"><b><i className="fa fa-globe fa-fw w3-margin-right w3-text-teal" />Idiomas</b></p>
                 {infos.idiomas.sort((a, b) => {
                     return b.width - a.width;
                 }).map((value, index) => {
                     return (
                         <Idiomas key={index} props={value}></Idiomas>
                     )
-                })}
+                })} */}
                 <br />
             </div>
         )
