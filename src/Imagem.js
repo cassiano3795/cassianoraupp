6a5afd6e2b0cbc41f07253f5c08ec46ceb098e18
Cassiano Raupp <cassiano.raupp@zeeng.com.br>
2019-06-10 14:36:28 -0300

    Ajustes.

--- src/Imagem.js
+++ src/Imagem.js
@@ -5,7 +5,7 @@ class Imagem extends Component {
     render() {
         return (
             <div className="w3-display-container">
-                <img src={infos.imagem} style={{ width: '100%' }} alt="Avatar" />
+                <img src={"profile.jpg"} style={{ width: '100%' }} alt="Avatar" />
                 <div className="w3-display-bottomleft w3-container w3-text-white">
                     <h2>Cassiano Raupp</h2>
                 </div>
